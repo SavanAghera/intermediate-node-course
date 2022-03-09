4bb22ffce14cb314070a2ed5fa186b8ced0fd12f
SavanAghera <savanpatelasr@gmail.com>
2022-03-09 23:42:14 +0530

    0

--- models/User.js
+++ models/User.js
@@ -4,6 +4,6 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
-});
+}); 
 
 module.exports= mongoose.model('User',UserSchema)
