d0743faf2f107eb6ca6ffc8283080775a3b0940a
SavanAghera <savanpatelasr@gmail.com>
2022-03-09 23:40:00 +0530

    add user model and connect to db

--- server.js
+++ server.js
@@ -89,6 +89,27 @@ app.route('/users/:id')
     )
   })
   // DELETE
-  .delete((req, res) => {
-    // User.findByIdAndDelete()
-  })
+ // DELETE
+.delete((req,res)=>{
+  User.findByIdAndDelete(
+    req.params.id,
+    (err,data)=>{
+      if (err){
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if (!data){
+        res.json({
+          success: false,
+          message: "Not Found"
+        })
+      } else {
+        res.json({
+          success: true,
+          data: data
+        })
+      }
+    }
+  )
+})
