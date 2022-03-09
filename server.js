e5d78638337aa364522b6cef0da8aab2d1998603
SavanAghera <savanpatelasr@gmail.com>
2022-03-09 23:04:33 +0530

    add user model and connect to db

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
+const User=require('./models/User');
 const port=8000;
 const app= express();
 
 app.use(bodyParser.json());
 
+async function connectToMongoDb() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/userData');
+}
+connectToMongoDb().then(() => console.log('connected to mongo')).catch(err => console.log(err));
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
